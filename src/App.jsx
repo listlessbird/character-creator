012f80c1ececfab012703b7fe6b734cfde979434
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-05-31 13:40:05 +0530

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
+   
       <CharacterEditor />
-      <Footer />
+      <Footer /> 
+      <div style={{
+      position: "absolute",
+      top:"60%",
+      width: "100%",
+      backgroundColor: "lightgrey",
+      zIndex: "-1",
+      height: "100%",
+    }} />
     </>
   );
 }
